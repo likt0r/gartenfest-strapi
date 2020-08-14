ec345af8d2542a517d168c0a2b27a603b7036ed4
bewr <bw@shoutrlabs.com>
2020-08-14 14:19:18 +0200

    moves all credentials to enviroment vars

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST"),
+  port: env.int("PORT"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '046ab8bac4eaf9166e8d7668707eb0a3'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
